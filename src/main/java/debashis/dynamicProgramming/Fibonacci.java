366160cc1d8a1c3d88fde74339cceac19ef43f84
Debashis Das <Debashis Das>
2022-08-15 22:11:33 +0530

    Debashis | added top to bottom and vice-versa approach

--- src/main/java/debashis/dynamicProgramming/Fibonacci.java
+++ src/main/java/debashis/dynamicProgramming/Fibonacci.java
@@ -1,23 +1,43 @@
 package debashis.dynamicProgramming;
 
+/**
+ * Memorization
+ */
 public class Fibonacci {
-
+    
     int[] hash;
 
     public static void main(String[] args){
         Fibonacci fibonacci = new Fibonacci();
         int n = 15;
         fibonacci.hash = new int[n];
-        System.out.println(fibonacci.nthFiboNumber(n));
+        System.out.println(fibonacci.nthFiboNumberTopToBottom(n));
+        System.out.println(fibonacci.nthFiboNumberBottomToTop(n));
     }
 
-    private int nthFiboNumber(int n){
+    // top to bottom
+    private int nthFiboNumberTopToBottom(int n){
         if(n<=2){
             return 1;
         }
         if(hash[n-1] == 0){
-            hash[n-1] = nthFiboNumber(n-1) + nthFiboNumber(n-2);
+            hash[n-1] = nthFiboNumberTopToBottom(n-1) + nthFiboNumberTopToBottom(n-2);
         }
         return hash[n-1];
     }
+
+    // bottom to top
+    private int nthFiboNumberBottomToTop(int n){
+        if(n <= 2){
+            return 1;
+        }
+        int init1 = 1,init2 = 1,result=0;
+        for(int i=3;i<=n;i++){
+            result = init1 + init2;
+            init2 = init1;
+            init1 = result;
+        }
+        return result;
+    }
+
 }
