963665372a78c051d204ef3a454ca083bad7dbaa
Debashis Das <Debashis Das>
2022-08-16 16:14:05 +0530

    Debashis | grid traveller

---
+++ src/main/java/debashis/dynamicProgramming/GridTraveller.java
@@ -0,0 +1,27 @@
+package debashis.dynamicProgramming;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class GridTraveller {
+
+    public static void main(String[] args){
+        GridTraveller gridTraveller = new GridTraveller();
+        int m = 18;
+        int n = 18;
+        Map<String,Long> memo = new HashMap<>();
+        System.out.println(gridTraveller.travel(m,n,memo));
+    }
+
+    private long travel(long m, long n,Map<String,Long> memo) {
+        if(m == 0 || n == 0)
+            return 0;
+        if(m == 1 && n ==1)
+            return 1;
+        String key = new StringBuilder().append(m).append(",").append(n).toString();
+        if(!memo.containsKey(key)){
+            memo.put(key,travel(m-1,n,memo) + travel(m,n-1,memo));
+        }
+        return memo.get(key);
+    }
+}
